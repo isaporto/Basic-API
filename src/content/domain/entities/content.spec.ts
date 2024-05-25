69da2cad9cd4cd90990d8fff88244fa19c755605
Isabela Porto <isamporto@hotmail.com>
2024-05-25 18:08:21 -0300

    Test: update Content Constructor unit test

--- src/content/domain/entities/content.spec.ts
+++ src/content/domain/entities/content.spec.ts
@@ -8,13 +8,45 @@ describe("Content Unit Tests", () => {
       thumbnail_url: "https://www.pudim.com.br/",
       content_url: "https://motherfuckingwebsite.com/"
     });
-    let props = omit(content.props, 'created_at');
-
+    let props = omit(content.props, "created_at");
     expect(props).toStrictEqual({
       title: "Stardew Valley",
       description: null,
       thumbnail_url: "https://www.pudim.com.br/",
       content_url: "https://motherfuckingwebsite.com/"
     });
+    expect(content.props.created_at).toBeInstanceOf(Date);
+
+    let created_at = new Date('12/12/2012');
+    content = new Content({
+      title: "To the Moon",
+      description: "A game to cry",
+      thumbnail_url: "https://pointerpointer.com/",
+      content_url: "https://store.steampowered.com/app/206440/To_the_Moon/",
+      created_at
+    });
+    expect(content.props).toStrictEqual({
+      title: "To the Moon",
+      description: "A game to cry",
+      thumbnail_url: "https://pointerpointer.com/",
+      content_url: "https://store.steampowered.com/app/206440/To_the_Moon/",
+      created_at
+    });
+
+    content = new Content({
+      title: "Life is Strange",
+      description: undefined,
+      thumbnail_url: "https://wallpapers.com/images/hd/life-is-strange-max-palm-open-x5uaalrzdsvzn8up.jpg",
+      content_url: "https://store.steampowered.com/app/319630/Life_is_Strange__Episode_1/",
+      created_at: null
+    });
+    props = omit(content.props, "created_at");
+    expect(props).toStrictEqual({
+      title: "Life is Strange",
+      description: null,
+      thumbnail_url: "https://wallpapers.com/images/hd/life-is-strange-max-palm-open-x5uaalrzdsvzn8up.jpg",
+      content_url: "https://store.steampowered.com/app/319630/Life_is_Strange__Episode_1/"
+    })
+    expect(content.props.created_at).toBeInstanceOf(Date);
   })
 })
