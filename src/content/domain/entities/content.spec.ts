e9dd97c45c1b293b323ebb8b39bb5c561534c4f2
Isabela Porto <isamporto@hotmail.com>
2024-05-25 18:40:04 -0300

    Feat: update description property from Content class to non optional and update Content unit tests

--- src/content/domain/entities/content.spec.ts
+++ src/content/domain/entities/content.spec.ts
@@ -2,51 +2,94 @@ import { Content } from "./content";
 import { omit } from "lodash";
 
 describe("Content Unit Tests", () => {
-  test('Constructor of content', () => {
+  test("Constructor of content", () => {
     let content = new Content({
       title: "Stardew Valley",
+      description: "Farming game",
       thumbnail_url: "https://www.pudim.com.br/",
-      content_url: "https://motherfuckingwebsite.com/"
+      content_url: "https://motherfuckingwebsite.com/",
     });
-    let props = omit(content.props, "created_at");
+    const props = omit(content.props, "created_at");
     expect(props).toStrictEqual({
       title: "Stardew Valley",
-      description: null,
+      description: "Farming game",
       thumbnail_url: "https://www.pudim.com.br/",
-      content_url: "https://motherfuckingwebsite.com/"
+      content_url: "https://motherfuckingwebsite.com/",
     });
     expect(content.props.created_at).toBeInstanceOf(Date);
 
-    let created_at = new Date('12/12/2012');
+    const created_at = new Date();
     content = new Content({
-      title: "To the Moon",
-      description: "A game to cry",
+      title: "To the moon",
+      description: "A game to cry over",
       thumbnail_url: "https://pointerpointer.com/",
-      content_url: "https://store.steampowered.com/app/206440/To_the_Moon/",
+      content_url: "localhost:400",
       created_at
     });
     expect(content.props).toStrictEqual({
-      title: "To the Moon",
-      description: "A game to cry",
+      title: "To the moon",
+      description: "A game to cry over",
       thumbnail_url: "https://pointerpointer.com/",
-      content_url: "https://store.steampowered.com/app/206440/To_the_Moon/",
+      content_url: "localhost:400",
       created_at
     });
+  });
 
-    content = new Content({
-      title: "Life is Strange",
-      description: undefined,
-      thumbnail_url: "https://wallpapers.com/images/hd/life-is-strange-max-palm-open-x5uaalrzdsvzn8up.jpg",
-      content_url: "https://store.steampowered.com/app/319630/Life_is_Strange__Episode_1/",
-      created_at: null
+  test("getter of title prop", () => {
+    const content = new Content({
+      title: "Don't Starve",
+      description: "some description",
+      thumbnail_url: "https://www.pudim.com.br/",
+      content_url: "https://www.pudim.com.br/",
     });
-    props = omit(content.props, "created_at");
-    expect(props).toStrictEqual({
-      title: "Life is Strange",
-      description: null,
-      thumbnail_url: "https://wallpapers.com/images/hd/life-is-strange-max-palm-open-x5uaalrzdsvzn8up.jpg",
-      content_url: "https://store.steampowered.com/app/319630/Life_is_Strange__Episode_1/"
-    })
-    expect(content.props.created_at).toBeInstanceOf(Date);
-  })
-})
+    expect(content.title).toBe("Don't Starve");
+  });
+
+  test("getter of description prop", () => {
+    const content = new Content({
+      title: "Don't Starve",
+      description: "some description",
+      thumbnail_url: "https://www.pudim.com.br/",
+      content_url: "https://www.pudim.com.br/",
+    });
+    expect(content.description).toBe("some description");
+  });
+
+  test("getter of thumbnail_url prop", () => {
+    const content = new Content({
+      title: "Don't Starve",
+      description: "some description",
+      thumbnail_url: "https://www.pudim.com.br/",
+      content_url: "https://www.pudim.com.br/",
+    });
+    expect(content.thumbnail_url).toBe("https://www.pudim.com.br/");
+  });
+
+  test("getter of content_url prop", () => {
+    const content = new Content({
+      title: "Don't Starve",
+      description: "some description",
+      thumbnail_url: "https://www.pudim.com.br/",
+      content_url: "https://www.pudim.com.br/",
+    });
+    expect(content.thumbnail_url).toBe("https://www.pudim.com.br/");
+  });
+
+  test("getter of created_at prop", () => {
+    const created_at = new Date('12/12/2012');
+    const props = {
+      title: "Don't Starve",
+      description: "some description",
+      thumbnail_url: "https://www.pudim.com.br/",
+      content_url: "https://www.pudim.com.br/",
+    };
+    let content = new Content({
+      ...props,
+      created_at,
+    });
+    expect(content.created_at).toBe(created_at);
+
+    content = new Content(props);
+    expect(content.created_at).toBeInstanceOf(Date);
+  });
+});
--- src/content/domain/entities/content.ts
+++ src/content/domain/entities/content.ts
@@ -1,6 +1,6 @@
 export type ContentProperties = {
   title: string,
-  description?: string,
+  description: string,
   thumbnail_url: string,
   content_url: string,
   created_at?: Date
@@ -8,18 +8,12 @@ export type ContentProperties = {
 
 export class Content {
   constructor(public readonly props: ContentProperties) {
-    this.description = this.props.description;
     this.props.created_at = this.props.created_at ?? new Date();
   }
 
   get title(){ return this.props.title };
-
   get description(){ return this.props.description };
-  private set description(value: string) { this.props.description = value ?? null };
-
   get thumbnail_url(){ return this.props.thumbnail_url };
-
   get content_url(){ return this.props.content_url };
-
   get created_at(){ return this.props.created_at };
 }
