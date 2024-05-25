208848df64acdf002e21a206651a4e8e5dfae95c
Isabela Porto <isamporto@hotmail.com>
2024-05-25 16:20:15 -0300

    Feat: Set the Content class properties and create its constructor test

--- src/content/domain/entities/content.spec.ts
+++ src/content/domain/entities/content.spec.ts
@@ -2,5 +2,20 @@ import { Content } from "./content"
 
 describe("Content Unit Tests", () => {
   test('Constructor of content', () => {
+    const created_at = new Date();
+    const content = new Content({
+      title: "Stardew Valley",
+      description: "Farming game",
+      thumbnail_url: "https://www.pudim.com.br/",
+      content_url: "https://motherfuckingwebsite.com/",
+      created_at,
+    });
+    expect(content.props).toStrictEqual({
+      title: "Stardew Valley",
+      description: "Farming game",
+      thumbnail_url: "https://www.pudim.com.br/",
+      content_url: "https://motherfuckingwebsite.com/",
+      created_at,
+    });
   })
 })
--- src/content/domain/entities/content.ts
+++ src/content/domain/entities/content.ts
@@ -1,1 +1,17 @@
-export class Content {}
+export type ContentProperties = {
+  title: string,
+  description: string,
+  thumbnail_url: string,
+  content_url: string,
+  created_at: Date
+}
+
+export class Content {
+  constructor(public readonly props: ContentProperties) {}
+
+  get title(){ return this.props.title };
+  get description(){ return this.props.description };
+  get thumbnail_url(){ return this.props.thumbnail_url };
+  get content_url(){ return this.props.content_url };
+  get created_at(){ return this.props.created_at };
+}
